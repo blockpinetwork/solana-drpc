865157753b079f986228c714e16296708ef09a53
yuanjun <yuanjun@bitopia.cn>
2021-10-13 11:40:25 +0800

    init

--- plugins/load_balancer/methods.go
+++ plugins/load_balancer/methods.go
@@ -16,7 +16,7 @@ func AddMethodInfo(methodName string, methodType int, methodGroup string) {
 	if len(methodGroup) > 0 {
 		group = methodGroup
 	}
-	methodMap["getProgramAccounts"] = &Method{Name:methodName, Type: methodType, Group: group}
+	methodMap[methodName] = &Method{Name:methodName, Type: methodType, Group: group}
 }
 
 func GetMethodInfo(methodName string) *Method {
